17fb69122b628938772403a24487c8061520ad21
MJakaKusuma <muhammadjaka031@gmail.com>
2026-06-01 14:00:51 +0700

    feat: update next.config.ts to allow local IPs for development

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // IZINKAN IP LOKAL ANDA UNTUK DEVELOPMENT DI JARINGAN
+  allowedDevOrigins: ["192.168.1.2", "192.168.1.2:3000", "localhost:3000"],
+  
+  // Jika ada konfigurasi lama Anda di bawah ini, silakan biarkan saja
 };
 
 export default nextConfig;
